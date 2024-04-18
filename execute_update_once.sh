fc76211319e763e96d9c24799acf03a026d89fa3
Fabian Grünwald <fabian.gruenwald@koenig-neurath.de>
2024-04-18 10:27:25 +0200

    script

--- execute_update_once.sh
+++ execute_update_once.sh
@@ -1,3 +1,7 @@
+#!/usr/bin/bash
 
+cd /home/adm_pdf2obs/LIVE/Ofml_Api/
 
-python -m scheduler_update_entry"/mnt/knps_testumgebung/ofml_development/repository"
+source venv/bin/activate
+
+python -m scheduler_update_entry "/mnt/knps_testumgebung/ofml_development/repository"
