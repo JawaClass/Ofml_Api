9efa8801b9e627f4000baa1d8101649bc5130fa5
JawaClass <draxenball95>
2024-08-20 17:24:21 +0200

    setup

--- setup.py
+++ setup.py
@@ -7,6 +7,7 @@ setup(
     version="1",
     author='Fabian Grünwald',
     author_email='fabian.gruenwald@koenig-neurath.de',
-    py_modules=find_packages(),
+    # py_modules=find_packages(),
+    packages=find_packages(),
     install_requires=["pandas"]
 )
