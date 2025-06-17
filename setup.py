0e43118d57234c05d29aec6f5cb3447384102f0f
Fabian Grünwald <fabian.gruenwald@koenig-neurath.de>
2025-06-17 14:48:50 +0200

    ofmtype literal type added

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup  # type:ignore
 
 setup(
     name="ofml_api",
-    version="1.0.2",
+    version="1.0.3",
     author="Fabian Grünwald",
     author_email="fabian.gruenwald@koenig-neurath.de",
     packages=["ofml_api"],
